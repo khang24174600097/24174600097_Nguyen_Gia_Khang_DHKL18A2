7a0b5de4977502914bc004fe81c20d5a911845f0
nguyen gia khang <ngkhang.24274600097@sv.uneti.edu.vn>
2024-10-21 22:24:23 +0700

    bai 4

---
+++ bài tập chương 5,6,7/bai4.py
@@ -0,0 +1,16 @@
+# Bài 4
+# Nhập thời gian
+t_g = float(input("Nhập thời gian (giờ): "))
+# Điều kiện thời gian
+if t_g > 0:
+    # Thông tin về bóng đèn
+    h_d_t = 220
+    cuong_do_dong_dien = 2.7
+    # Phép tính
+    cong_suat = t_g * h_d_t * cuong_do_dong_dien / 3600 * 1000
+    # Tính
+    phai_tra = cong_suat * 7000
+    # In kết quả
+    print("Tiền phải trả là:", phai_tra)
+else:
+    print("Thời gian sử dụng phải lớn hơn 0.")
