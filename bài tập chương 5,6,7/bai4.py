62b040b3119bdeec18360c1553cda5c12f40f843
nguyen gia khang <ngkhang.24274600097@sv.uneti.edu.vn>
2024-10-24 11:33:46 +0700

    bai 4

--- bài tập chương 5,6,7/bai4.py
+++ bài tập chương 5,6,7/bai4.py
@@ -1,13 +1,13 @@
 # Bài 4
 # Nhập thời gian
-t_g = float(input("Nhập thời gian (giờ): "))
+t_g = float(input("Nhập thời gian (giây): "))
 # Điều kiện thời gian
 if t_g > 0:
     # Thông tin về bóng đèn
     h_d_t = 220
     cuong_do_dong_dien = 2.7
     # Phép tính
-    cong_suat = t_g * h_d_t * cuong_do_dong_dien / 3600 * 1000
+    cong_suat = t_g * h_d_t * cuong_do_dong_dien / (3600 * 1000)
     # Tính
     phai_tra = cong_suat * 7000
     # In kết quả
